5d761b3b06e40d184e3dcdf7dd13c98976b559e5
Saurabh Singh <saurabhsinghup123@gmail.com>
2023-08-14 17:40:44 +0530

    taskcardone

--- task1/src/components/TaskCard.js
+++ task1/src/components/TaskCard.js
@@ -1,20 +1,270 @@
-import React from 'react';
-import './TaskCard.css';
+import React from "react";
+import "./TaskCard.css";
 
 const TaskCard = () => {
   return (
     <div className="task-card">
-      <div className="task-title">Upcoming Task</div>
       <div className="task-content">
-        <div className="task-item">Task 1</div>
-        <div className="task-item">Task 2</div>
-        <div className="task-item">Task 3</div>
+        <div className="task-title">
+          <div className="title">Upcoming Task</div>
+          <div className="icon-title">
+            <span className="icon-add"></span>
+            <span className="icon-options"></span>
+          </div>
+        </div>
+        <div className="items">
+          <div className="itemheader">
+            <div className="header">
+              <span className="uidesign">UI Design</span>
+              <span className="research">Research</span>
+            </div>
+            <span className="icon-options"> </span>
+          </div>
+          <div className="itemcontent"></div>
+          <div className="heading2">Research Featured Order</div>
+          <div className="heading3">
+            An order feature is needed for <br></br> users when purchases
+          </div>
+          <div className="date">
+            <span className="icon-calendar"></span>
+            <span className="due">Due to:</span>
+            <span className="dateinside">14 May</span>
+          </div>
+          <div className="line6"></div>
+          <div className="underline">
+            <div className="imagecircle1">
+              <img
+                src="https://lh3.googleusercontent.com/a/AAcHTteOKEozjO4IJrLtr0OS0n_XUnKkqXfS7Ejkke00Sa2CkWQ=s96-c"
+                alt="Image1"
+                className="overlap-image1"
+              />
+              <img
+                src="https://lh3.googleusercontent.com/a/AAcHTteOKEozjO4IJrLtr0OS0n_XUnKkqXfS7Ejkke00Sa2CkWQ=s96-c"
+                alt="Image2"
+                className="overlap-image2"
+              />
+              <div className="plus-icon1">
+                <span className="icon-add"></span>
+                <span className="five">1</span>
+              </div>
+            </div>
+            <div className="iconunder">
+              <div className="comment">
+                <span className="icon-comments"></span>
+                <span>2</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="items">
+          <div className="itemheader">
+            <div className="header">
+              <span className="uidesign">UI Design</span>
+              <span className="apps">Mobile Apps</span>
+            </div>
+            <span className="icon-options"> </span>
+          </div>
+          <div className="itemcontent"></div>
+          <div className="heading2">Design System</div>
+          <div className="heading3">
+          Making a system design from the
+dashboard that has been made, we make a
+light mode and dark mode version
+          </div>
+          <div className="date">
+            <span className="icon-calendar"></span>
+            <span className="due">Due to:</span>
+            <span className="dateinside">14 May</span>
+          </div>
+          <div className="line6"></div>
+          <div className="underline">
+            <div className="imagecircle1">
+              <img
+                src="https://lh3.googleusercontent.com/a/AAcHTteOKEozjO4IJrLtr0OS0n_XUnKkqXfS7Ejkke00Sa2CkWQ=s96-c"
+                alt="Image1"
+                className="overlap-image1"
+              />
+              <img
+                src="https://lh3.googleusercontent.com/a/AAcHTteOKEozjO4IJrLtr0OS0n_XUnKkqXfS7Ejkke00Sa2CkWQ=s96-c"
+                alt="Image2"
+                className="overlap-image2"
+              />
+              <div className="plus-icon1">
+                <span className="icon-add"></span>
+                <span className="five">1</span>
+              </div>
+            </div>
+            <div className="iconunder">
+              <div className="comment">
+                <span className="icon-comments"></span>
+                <span>1</span>
+              </div>
+              <div className="clip">
+                <span className="icon-clip"></span>
+                <span>3</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="task-title">Work in Progress</div>
       <div className="task-content">
+        <div className="task-title">
+          <div className="title">Work in Progress</div>
+          <div className="icon-title">
+            <span className="icon-add"></span>
+            <span className="icon-options"></span>
+          </div>
+        </div>
+        <div className="items">
+          <div className="itemheader">
+            <div className="header">
+              <span className="uidesign">UI Design</span>
+              <span className="website">Website</span>
+            </div>
+            <span className="icon-options"> </span>
+          </div>
+          <div className="itemcontent"></div>
+          <div className="heading2">High Fidelity - Website</div>
+          <div className="heading3">
+            Make high fidelity based on the <br />
+            style that the client has given, <br /> and try exploring based on
+            the moodboard.
+          </div>
+
+          <img
+            className="image1"
+            src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT40F9i4Tuq0eux3sasbgBN0tnYvMFAIKmSmOXzoevpq07G_95h"
+            alt="abc"
+          ></img>
+          <div className="line6"></div>
+          <div className="underline">
+            <div className="imagecircle1">
+              <img
+                src="https://lh3.googleusercontent.com/a/AAcHTteOKEozjO4IJrLtr0OS0n_XUnKkqXfS7Ejkke00Sa2CkWQ=s96-c"
+                alt="Image1"
+                className="overlap-image1"
+              />
+              <img
+                src="https://lh3.googleusercontent.com/a/AAcHTteOKEozjO4IJrLtr0OS0n_XUnKkqXfS7Ejkke00Sa2CkWQ=s96-c"
+                alt="Image2"
+                className="overlap-image2"
+              />
+              <div className="plus-icon1">
+                <span className="icon-add"></span>
+                <span className="five">1</span>
+              </div>
+            </div>
+            <div className="iconunder">
+              <div className="comment">
+                <span className="icon-comments"></span>
+                <span>3</span>
+              </div>
+              <div className="clip">
+                <span className="icon-clip"></span>
+                <span>6</span>
+              </div>
+            </div>
+          </div>
+        </div>
         
-        <div className="task-item">Work 1</div>
-        <div className="task-item">Work 2</div>
+      </div>
+      <div className="task-content">
+        <div className="task-title">
+          <div className="title">Review Task</div>
+          <div className="icon-title">
+            <span className="icon-add"></span>
+            <span className="icon-options"></span>
+          </div>
+        </div>
+        <div className="items">
+          <div className="itemheader">
+            <div className="header">
+              <span className="development">Development</span>
+              <span className="apps">Mobile Apps</span>
+            </div>
+            <span className="icon-options"> </span>
+          </div>
+          <div className="itemcontent"></div>
+          <div className="heading2">Research Featured Order</div>
+          <div className="heading3">
+            An order feature is needed for <br></br> users when purchases
+          </div>
+          <div className="line6"></div>
+          <div className="underline">
+            <div className="imagecircle1">
+              <img
+                src="https://lh3.googleusercontent.com/a/AAcHTteOKEozjO4IJrLtr0OS0n_XUnKkqXfS7Ejkke00Sa2CkWQ=s96-c"
+                alt="Image1"
+                className="overlap-image1"
+              />
+              <img
+                src="https://lh3.googleusercontent.com/a/AAcHTteOKEozjO4IJrLtr0OS0n_XUnKkqXfS7Ejkke00Sa2CkWQ=s96-c"
+                alt="Image2"
+                className="overlap-image2"
+              />
+              <div className="plus-icon1">
+                <span className="icon-add"></span>
+                <span className="five">1</span>
+              </div>
+            </div>
+            <div className="iconunder">
+              <div className="comment">
+                <span className="icon-comments"></span>
+                <span>7</span>
+              </div>
+              <div className="clip">
+                <span className="icon-clip"></span>
+                <span>3</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="items">
+          <div className="itemheader">
+            <div className="header">
+              <span className="uidesign">UI Design</span>
+              <span className="website">Website</span>
+            </div>
+            <span className="icon-options"> </span>
+          </div>
+          <div className="itemcontent"></div>
+          <div className="heading2">Lardiro V3 - V&bsite</div>
+          <div className="heading3">
+          Create a 3rd version of the landing page
+with a different style from the style that the
+client has provided. The style is neat but
+doesn't seem stiff.
+          </div>
+          <div className="line6"></div>
+          <div className="underline">
+            <div className="imagecircle1">
+              <img
+                src="https://lh3.googleusercontent.com/a/AAcHTteOKEozjO4IJrLtr0OS0n_XUnKkqXfS7Ejkke00Sa2CkWQ=s96-c"
+                alt="Image1"
+                className="overlap-image1"
+              />
+              <img
+                src="https://lh3.googleusercontent.com/a/AAcHTteOKEozjO4IJrLtr0OS0n_XUnKkqXfS7Ejkke00Sa2CkWQ=s96-c"
+                alt="Image2"
+                className="overlap-image2"
+              />
+              <div className="plus-icon1">
+                <span className="icon-add"></span>
+                <span className="five">1</span>
+              </div>
+            </div>
+            <div className="iconunder">
+              <div className="comment">
+                <span className="icon-comments"></span>
+                <span>2</span>
+              </div>
+              <div className="clip">
+                <span className="icon-clip"></span>
+                <span>1</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
